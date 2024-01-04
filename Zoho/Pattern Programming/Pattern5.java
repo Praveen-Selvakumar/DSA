266aa767836aa1120c3b3766de3d5e2d09efe26a
Develoepers_Look <91062612+Praveen-Selvakumar@users.noreply.github.com>
2024-01-04 09:23:48 +0530

    Add files via upload

---
+++ Zoho/Pattern Programming/Pattern5.java
@@ -0,0 +1,44 @@
+import java.util.*;
+   
+   
+   
+@SuppressWarnings("unchecked")
+class Temp{
+ 
+	 
+    
+
+  public static void main(String[] args){
+	  
+	     
+		 
+		 /*
+		 * * * * *
+		   * * * *
+		     * * *
+		       * *
+                 *
+		 
+		 */
+		            
+	
+	           int i = 0, j =0; 
+				
+				while(i < 5){
+					while(j < 5){
+						 if(j >= i){
+							 System.out.print("* ");
+						 }else{
+							 System.out.print("  ");
+						 }
+						 j++;
+					} i++;
+					j=0;
+					System.out.println();
+				}
+				 
+			   
+  } 
+   
+   
+}
