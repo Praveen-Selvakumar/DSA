18110e1747a0f02c2141b98815cef3e9d0961713
Develoepers_Look <91062612+Praveen-Selvakumar@users.noreply.github.com>
2024-01-08 23:09:55 +0530

    Create Pattern9.java

---
+++ Zoho/Pattern Programming/Pattern9.java
@@ -0,0 +1,33 @@
+import java.util.*;
+   
+   
+   
+@SuppressWarnings("unchecked")
+class Temp{
+ 
+ 
+  
+
+  public static void main(String[] args){
+	 
+	 Temp cn  = new Temp();
+	 
+	 
+	         int i =1, j =1;
+			 
+			 while(i <= 5){
+				 while(j <= 5){
+					 System.out.print(i+" ");
+					 j++;
+				 }
+				 System.out.println();
+				 j=1;
+				 i++;
+			 }
+			 
+	 
+	 
+	 
+	  
+  }
+}
