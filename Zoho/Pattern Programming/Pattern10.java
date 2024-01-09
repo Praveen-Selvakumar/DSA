fbf4dd3af4dbe3d596ff8013cdeb1ac7821a82f0
Develoepers_Look <91062612+Praveen-Selvakumar@users.noreply.github.com>
2024-01-09 21:59:02 +0530

    Create Pattern10.java

---
+++ Zoho/Pattern Programming/Pattern10.java
@@ -0,0 +1,30 @@
+import java.util.*;
+   
+   
+   
+@SuppressWarnings("unchecked")
+class Pattern10{
+	 
+	
+	
+	public static void main(String[] args){
+		   Temp cn  = new Temp();
+		   
+		   int i=1, j=1;
+		   
+		   while(i<=5){
+			   while(j<=5){
+				   System.out.print(j+" ");
+				   j++;
+			   }
+			   i++;
+			   j=1;
+			   System.out.println();
+		   }
+		    
+ 		 
+		 
+	}
+  
+	  
+ }
