fa8431b606fd2d4b9bf8a5ddd980c6f5f26ae73f
Develoepers_Look <91062612+Praveen-Selvakumar@users.noreply.github.com>
2024-01-11 22:45:05 +0530

    Create Pattern12.java

---
+++ Zoho/Pattern Programming/Pattern12.java
@@ -0,0 +1,28 @@
+import java.util.*;
+   
+   
+   
+@SuppressWarnings("unchecked")
+class Pattern12{
+	 
+	
+	
+	public static void main(String[] args){
+		   Temp cn  = new Temp();
+		   
+		   int i=5, j=5;
+		   
+		   while(i>=1){
+			   while(j>=1){
+				   System.out.print(i+" "); 
+				   j--;
+			   } 
+			   j=5;
+			   i--;
+			   System.out.println();
+		   }
+		    
+ 		 
+		 
+	}	  
+ }
