a042aac1d540835f58b9d5a2d79b01aa78f682c5
Develoepers_Look <91062612+Praveen-Selvakumar@users.noreply.github.com>
2024-01-14 23:22:45 +0530

    Create Pattern14.java

---
+++ Zoho/Pattern Programming/Pattern14.java
@@ -0,0 +1,30 @@
+import java.util.*;
+   
+   
+   
+@SuppressWarnings("unchecked")
+class Pattern14{ 
+	 
+	
+	public static void main(String[] args){
+		   Temp cn  = new Temp();
+		   
+		   int i =1, j =1;
+		   
+		   while(i<=5){
+			   while(j<=i){
+				  System.out.print(j+" ");
+				  j++;
+			   }		
+			   System.out.println();
+			   i++;
+			   j=1;
+		   }
+		   
+				
+		 
+	}	
+
+
+	
+ }
