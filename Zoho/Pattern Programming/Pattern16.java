1269a6e704c9c5cb7c91f0fc8096d2ae276fe81c
Develoepers_Look <91062612+Praveen-Selvakumar@users.noreply.github.com>
2024-01-16 22:44:31 +0530

    Create Pattern16.java

---
+++ Zoho/Pattern Programming/Pattern16.java
@@ -0,0 +1,32 @@
+import java.util.*;
+   
+   
+   
+@SuppressWarnings("unchecked")
+class Pattern16{
+	  
+	public static void main(String[] args){
+		   Pattern16 cn  = new Pattern16();
+		   
+		   
+		   int i = 5 , j =1;
+		   
+		   
+		   while(i>=1){
+			   while(j<=i){
+				    if(j<=i){
+						System.out.print(j+" ");
+					}else{
+					 System.out.print("  ");
+					}
+					j++;
+			   } 
+			   i--;
+			   j=1;
+			   System.out.println();
+		   }
+		    
+				
+		 
+	}		
+ }
