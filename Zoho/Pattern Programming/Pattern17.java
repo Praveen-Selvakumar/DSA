1a27c02cc0f5fd7f32fb67555ee3bd02b719b533
Develoepers_Look <91062612+Praveen-Selvakumar@users.noreply.github.com>
2024-01-18 07:44:47 +0530

    Create Pattern17.java

---
+++ Zoho/Pattern Programming/Pattern17.java
@@ -0,0 +1,46 @@
+import java.util.*;
+   
+   
+   
+@SuppressWarnings("unchecked")
+class Pattern17{
+	 
+	 
+	
+	public static void main(String[] args){
+		   Pattern17 cn  = new Pattern17();
+		   
+		   
+		   int i = 5 , j =5;
+		   
+		   
+		   while(i>=1){
+			   while(j>=1){
+				   if(j<=i){
+					   System.out.print(j+" ");
+				   }else{
+					    System.out.print("  ");
+				   } 
+				   j--;
+			   } 		
+                 j=5;
+				 i--;
+				 System.out.println();
+                 
+				 
+		   }
+		   
+		  
+		  
+		   
+		  
+		  
+		  
+		 
+				
+		 
+	}	
+
+
+	
+ }
